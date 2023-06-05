ee7dc46080160d40bd3688134e220752b817d030
Eduardo PINA <e.pina@adlperformance.fr>
2023-06-05 16:38:32 +0200

    add all stacks

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,9 +5,8 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello piza world how are you eeekhh!!! fff!</h1>
-                <h1>Hello world how are you  fff!</h1>
-                <h1>YEAH!!!</h1>
+
+                <h1>Welcome to this react app !!!!!!</h1>
                 <h1>{process.env.NODE_ENV}</h1>
             </div>
         );
